3b48b5bce3e164ac8bdb62a2c92ace14f09a14d5
noveky <eznoveky@gmail.com>
2024-12-06 07:33:31 +0900

    remove: deprecated model classes

--- src/backend/models/models.py
+++ src/backend/models/models.py
@@ -6,31 +6,6 @@ ContentType = typing.Literal["image", "text"] | None
 
 
 @dataclass
-class Triple:
-    triple_id: uuid.UUID
-    item_id: uuid.UUID
-    attr: str
-    attr_embedding_vector: np.ndarray
-    value: str
-    value_embedding_vector: np.ndarray
-
-
-@dataclass
-class Item:
-    item_id: uuid.UUID
-    props: dict
-
-    def update_props(self, triple: Triple):
-        self.props[triple.attr] = triple.value
-
-    def __repr__(self):
-        all_props = self.props
-        all_props.update({"id": self.item_id})
-        prop_list_str = ", ".join([f"{k}={repr(v)}" for k, v in all_props.items()])
-        return f"Item({prop_list_str})"
-
-
-@dataclass
 class File:
     id: str
     path: str
